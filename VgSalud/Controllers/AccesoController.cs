c8adc9f801616bce6f2bd6c05cfd429dd28a1cec
luis <albert.malorca@gmail.com>
2017-06-01 11:09:53 -0500

    21321

--- VgSalud/Controllers/AccesoController.cs
+++ VgSalud/Controllers/AccesoController.cs
@@ -12,7 +12,7 @@ namespace VgSalud.Controllers
 {
     public class AccesoController : Controller
     {
-        //Actualizo12345   ----152-----ACTUALIZO11 ----accccc
+        //Actualizo12345   ----152-----ACTUALIZO11 ----accccc111
     
         public List<E_Acceso> ListaModulos(string CodUsu)
         {
