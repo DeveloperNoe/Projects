29560fa08d18c69d0978ddfa66af1f7776b644f8
Noe Jhon Camavilca <noejhon90@gmail.com>
2017-06-01 09:53:36 -0500

    10

--- VgSalud/Controllers/AccesoController.cs
+++ VgSalud/Controllers/AccesoController.cs
@@ -12,7 +12,7 @@ namespace VgSalud.Controllers
 {
     public class AccesoController : Controller
     {
-        //Actualizo1
+        //Actualizo123
     
         public List<E_Acceso> ListaModulos(string CodUsu)
         {
